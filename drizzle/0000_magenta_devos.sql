9059d3fc2ec8fba1d687aed7da20a8e90de4b0de
Effar <2942325-Effar@users.noreply.replit.com>
2026-01-04 15:45:07 +0000

    Restored to '0eeadd5bfa508b65563092e307e1065bac082ef0'
    Replit-Restored-To: 0eeadd5bfa508b65563092e307e1065bac082ef0

--- drizzle/0000_magenta_devos.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE "predictions" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"sepal_length" real NOT NULL,
-	"sepal_width" real NOT NULL,
-	"petal_length" real NOT NULL,
-	"petal_width" real NOT NULL,
-	"predicted_species" text NOT NULL,
-	"created_at" timestamp DEFAULT now() NOT NULL
-);
